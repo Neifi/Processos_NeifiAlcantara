bb98d33212a8dc704b8d38c35427f32cf58ac472
Neifi <44041549+Neifi@users.noreply.github.com>
2019-10-30 19:14:18 +0100

    Add files via upload

---
+++ PracticaProcessos.py
@@ -0,0 +1,26 @@
+from multiprocessing import Process
+import datetime
+import time
+import os
+
+datetime.datetime(2009, 1, 6, 15, 8, 24)
+
+def t(s):
+    while True:
+        time.sleep(s)
+        print(datetime.datetime.now())
+        
+
+
+def main():
+    process = Process(target=t, args=(1, ))
+    process.start()
+    for i in range(10):
+        time.sleep(0.5)
+        print(process.pid)
+    process.terminate()
+    print("FIN")
+      
+       
+
+main()
